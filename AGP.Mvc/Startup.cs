9e4938ef632c65f96a842bef1deb77cb56d6d4b5
mobin <mahdihassani9374@gmail.com>
2018-09-26 12:43:41 +0330

    develop of Game

--- AGP.Mvc/Startup.cs
+++ AGP.Mvc/Startup.cs
@@ -46,6 +46,7 @@ namespace AGP.Mvc
             // repository and Servcie Injection
             services.AddScoped<UserManager>();
             services.AddScoped<LogServiceRepository>();
+            services.AddScoped<GameRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
