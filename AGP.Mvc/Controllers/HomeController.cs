b980fa6cf83f78284a8622676a3c96ec454cc0d4
mahdi <mobinhassani9374@gmail.com>
2018-09-28 15:59:32 +0330

    about us

--- AGP.Mvc/Controllers/HomeController.cs
+++ AGP.Mvc/Controllers/HomeController.cs
@@ -13,5 +13,10 @@ namespace AGP.Mvc.Controllers
            
             return View();
         }
+
+        public IActionResult AboutUs()
+        {
+            return View();
+        }
     }
 }
