20f394cc8d120b99a021983b52e36df1755dcf26
mobin <mahdihassani9374@gmail.com>
2018-09-28 20:37:53 +0330

    extenstionMethods

---
+++ AGP.Utility/ExtensionMethods/DateTimeExtensionMethod.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AGP.Utility.ExtensionMethods
+{
+    public static class DateTimeExtensionMethod
+    {
+        public static string ToShortTime(this DateTime date)
+        {
+            return $"{date.Year} / {date.Month} / {date.Day}";
+        }
+    }
+}
