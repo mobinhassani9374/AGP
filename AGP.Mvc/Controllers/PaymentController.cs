f8afad303314ff2bc359c4ca2d40813560038245
mobinhassani <mobinhassani9374@gail.com>
2018-10-14 08:15:15 +0330

    Payment Controller

---
+++ AGP.Mvc/Controllers/PaymentController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AGP.Mvc.Controllers
+{
+    public class PaymentController : Controller
+    {
+        /// <summary>
+        /// نتیجه از سمت بیت پی
+        /// </summary>
+        /// <param name="trans_id"></param>
+        /// <param name="id_get"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public IActionResult BitPayResponse(int trans_id, int id_get)
+        {
+            return View();
+        }
+    }
+}
