beb642c19d2ffc925ae569b04f1c37b76e14d93f
mobinhassani <mobinhassani9374@gmail.com>
2018-10-15 11:39:06 +0330

    add AccountGameBuyResultDto

---
+++ AGP.Domain/DTO/AccountGame/AccountGameBuyResultDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AGP.Domain.DTO.AccountGame
+{
+    public class AccountGameBuyResultDto
+    {
+        public bool Conflict { get; set; }
+        public string Message { get; set; }
+        public static AccountGameBuyResultDto Error()
+        {
+            return new AccountGameBuyResultDto
+            {
+                Conflict = false,
+                Message = "اکانت مورد نظر توسط کاربر دیگری در حال بررسی می باشد"
+            };
+        }
+    }
+}
